73ba9b71a3295a02b93e1e001ed0eebcab1c9322
Jay Panchal <84466768+Prithvi25298@users.noreply.github.com>
2024-09-16 09:18:48 +0530

    Add files via upload

---
+++ AdminController.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Newtonsoft.Json;
+using SchoolPortalV2.Models;
+using SchoolPortalV2.Operations;
+
+namespace SchoolPortalV2.Controllers
+{
+    public class AdminController : Controller
+    {
+        DbOperations opretions = new DbOperations();
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        #region Users
+        public ActionResult Users()
+        {
+            UserList users = new UserList();
+            try
+            {
+                users.dsUsers = opretions.GetList("_Get_Teacher_List");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View(users);
+        }
+        [HttpPost]
+        public ActionResult SaveUsers(User_Basic_Details ubd)
+        {
+            string Status = "";
+            string msg = "";
+            string outputMsg = "";
+            string viewContentHtml = "";
+            try
+            {
+                Int64 IsError = opretions.SaveModelData(ubd, "Save_User_Basic_Details", out outputMsg);
+                if (IsError > 0)
+                {
+                    Status = "SUCCESS";
+                    if (ubd.usr_id > 0) { msg = "updated"; }
+                    else { msg = "added"; }
+                }
+                else if (IsError == -1)
+                {
+                    Status = "DUPLICATE";
+                    msg = "duplicate";
+                }
+                else
+                {
+                    Status = "ERROR";
+                }
+                DataSet ds = opretions.GetList("_Get_Teacher_List");
+                viewContentHtml = opretions.RenderViewToString("Users_Partial", ds, ControllerContext);
+                return Json(new
+                {
+                    Status = Status,
+                    Message = msg,
+                    ViewContentHtml = viewContentHtml
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    Status = "ERROR",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditUsers(string id)
+        {
+            string Status = "";
+            string msg = "";
+            string entityTeacher = "";
+            try
+            {
+                List<SqlParameter> parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter("@usrId", id));
+                DataTable dt = opretions.GetDetailsForEdit("_Get_Teacher_List", parameters);
+                if (dt.Rows.Count > 0)
+                {
+                    Status = "SUCCESS";
+                    entityTeacher = JsonConvert.SerializeObject(dt);
+                }
+                else
+                {
+                    Status = "ERROR";
+                }
+                return Json(new
+                {
+                    Status = Status,
+                    Message = msg,
+                    entity = entityTeacher
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    Status = "ERROR",
+                    Message = ex.Message
+                });
+            }
+        }
+        public ActionResult UsersAllDetails(string id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SaveUserDetails(User_Details ud)
+        {
+            string Status = "";
+            string msg = "";
+            string outputMsg = "";
+            string viewContentHtml = "";
+            try
+            {
+                Int64 IsError = opretions.SaveModelData(ud, "_Save_User_Detail", out outputMsg);
+                if (IsError > 0)
+                {
+                    Status = "SUCCESS";
+                    if (ud.usr_id > 0) { msg = "updated"; }
+                    else { msg = "added"; }
+                }
+                else if (IsError == -1)
+                {
+                    Status = "DUPLICATE";
+                    msg = "duplicate";
+                }
+                else
+                {
+                    Status = "ERROR";
+                }
+                DataSet ds = opretions.GetList("_Get_Teacher_List");
+                viewContentHtml = opretions.RenderViewToString("Users_Partial", ds, ControllerContext);
+                return Json(new
+                {
+                    Status = Status,
+                    Message = msg,
+                    ViewContentHtml = viewContentHtml
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    Status = "ERROR",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        public ActionResult UsersDetails() { return View(); }
+
+        #endregion
+
+        #region Student
+        public ActionResult Student()
+        {
+            StudentList student = new StudentList();
+            try
+            {
+                student.dsStudent = opretions.GetList("_Get_Student_List");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View(student);
+        }
+        public ActionResult StudentAllDetails(int id)
+        {
+            StudentList student = new StudentList();
+            try
+            {
+                student.dsStudent = opretions.GetList("_Get_Student_List");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult SaveStudent(Student_Basic_Details std)
+        {
+            string Status = "";
+            string msg = "";
+            string outputMsg = "";
+            string viewContentHtml = "";
+            try
+            {
+                Int64 IsError = opretions.SaveModelData(std, "Save_Student_Basic_Details", out outputMsg);
+                if (IsError > 0)
+                {
+                    Status = "SUCCESS";
+                    if (std.std_id > 0) { msg = "updated"; }
+                    else { msg = "added"; }
+                }
+                else if (IsError == -1)
+                {
+                    Status = "DUPLICATE";
+                    msg = "duplicate";
+                }
+                else
+                {
+                    Status = "ERROR";
+                }
+                DataSet ds = opretions.GetList("_Get_Student_List");
+                viewContentHtml = opretions.RenderViewToString("Student_Partial", ds, ControllerContext);
+                return Json(new
+                {
+                    Status = Status,
+                    Message = msg,
+                    ViewContentHtml = viewContentHtml
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    Status = "ERROR",
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditStudent(int std_id)
+        {
+            string Status = "";
+            string msg = "";
+            string entityStudent = "";
+            try
+            {
+                List<SqlParameter> parameters = new List<SqlParameter>();
+                parameters.Add(new SqlParameter("@std_id", std_id));
+                DataTable dtStudent = opretions.GetDetailsForEdit("_Get_Student_Details", parameters);
+                if (dtStudent.Rows.Count > 0)
+                {
+                    Status = "SUCCESS";
+                    entityStudent = JsonConvert.SerializeObject(dtStudent);
+                }
+                else
+                {
+                    Status = "ERROR";
+                }
+                return Json(new
+                {
+                    Status = Status,
+                    Message = msg,
+                    entity = entityStudent
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    Status = "ERROR",
+                    Message = ex.Message
+                });
+            }
+        }
+        #endregion
+
+        #region Attendance
+        public ActionResult Attendance()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
---
+++ DbOperations.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace SchoolPortalV2.Operations
+{
+    public class DbOperations
+    {
+        // Retrieve connection string from your application configuration file (Web.config or App.config)
+        string connectionString = ConfigurationManager.ConnectionStrings["SchoolDBConnectionString"].ConnectionString;
+        public DataSet GetList(string spname)
+        {
+            DataSet ds = new DataSet();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(spname, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                // Create a SqlDataAdapter to fetch data into the DataSet
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                // Open the connection
+                connection.Open();
+                // Fill the DataSet with data
+                adapter.Fill(ds);
+            }
+            return ds;
+        }
+        public DataSet GetListWithParameters(string spname,List<SqlParameter> parms)
+        {
+            DataSet dataSet = new DataSet();
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(spname, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure; // or CommandType.Text if it’s a raw query
+
+                    if (parms != null)
+                    {
+                        foreach (SqlParameter param in parms)
+                        {
+                            command.Parameters.Add(param);
+                        }
+                    }
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataSet);
+                }
+            }
+
+            return dataSet;
+        }
+        public DataTable GetDetailsForEdit(string spname, List<SqlParameter> parameters)
+        {
+            // Connection string to your SQL Server database
+            string connectionString = ConfigurationManager.ConnectionStrings["SchoolDBConnectionString"].ConnectionString;
+            // Create a new DataTable to hold the result
+            DataTable dataTable = new DataTable();
+            try
+            {
+                // Create a new SqlConnection using the connection string
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Create a new SqlCommand
+                    using (SqlCommand cmd = new SqlCommand(spname, connection))
+                    {
+                        // Set the command type to stored procedure
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Add parameters to the command
+                        if (parameters != null)
+                        {
+                            foreach (SqlParameter parameter in parameters)
+                            {
+                                cmd.Parameters.Add(parameter);
+                            }
+                        }
+
+                        // Create a new SqlDataAdapter
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            // Fill the DataTable with the data from the stored procedure
+                            adapter.Fill(dataTable);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions here, like logging or throwing
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            // Return the populated DataTable
+            return dataTable;
+        }
+        public int SaveModelData<T>(T model, string storedProcedureName,out string ErrorMsg)
+        {
+            var properties = typeof(T).GetProperties();
+            List<SqlParameter> parameters = new List<SqlParameter>();
+
+            foreach (var property in properties)
+            {
+                parameters.Add(new SqlParameter("@" + property.Name, property.GetValue(model) ?? DBNull.Value));
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddRange(parameters.ToArray());
+
+                // Add output parameters
+                SqlParameter isErrorParameter = new SqlParameter("@IsError", SqlDbType.Int);
+                isErrorParameter.Direction = ParameterDirection.Output;
+                command.Parameters.Add(isErrorParameter);
+
+                SqlParameter errorMsgParameter = new SqlParameter("@ErrorMsg", SqlDbType.VarChar, 100);
+                errorMsgParameter.Direction = ParameterDirection.Output;
+                command.Parameters.Add(errorMsgParameter);
+
+                // Executing the stored procedure
+                command.ExecuteNonQuery();
+                int isError = Convert.ToInt32(isErrorParameter.Value);
+                ErrorMsg = errorMsgParameter.Value.ToString();
+                connection.Close();
+                return isError;
+            }           
+        }
+
+        public string RenderViewToString(string viewName, object model, ControllerContext controllerContext)
+        {
+            controllerContext.Controller.ViewData.Model = model;
+
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(controllerContext, viewName);
+                var viewContext = new ViewContext(controllerContext, viewResult.View, controllerContext.Controller.ViewData, controllerContext.Controller.TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(controllerContext, viewResult.View);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
